0779d63a27ed5fe290e7e26db037bcab724f2904
chosey0 <csq1404@gmail.com>
2022-04-11 15:00:25 +0900

    Roman to Interger ~ing

--- leetcode/romanToInt.js
+++ leetcode/romanToInt.js
@@ -2,15 +2,27 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
+var romanToInt = function (s) {
   let I, V, X, L, C, D, M;
-  const excNum = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000
+  let n;
+  let RomanNum = s.split('');
+  let Operation = 0;
+
+  for (n = 0; n < RomanNum.length; n++) {
+    if (RomanNum[n] == 'I') RomanNum[n] = 1;
+    else if (RomanNum[n] == 'V') RomanNum[n] = 5;
+    else if (RomanNum[n] == 'X') RomanNum[n] = 10;
+    else if (RomanNum[n] == 'L') RomanNum[n] = 50;
+    else if (RomanNum[n] == 'C') RomanNum[n] = 100;
+    else if (RomanNum[n] == 'D') RomanNum[n] = 500;
+    else if (RomanNum[n] == 'M') RomanNum[n] = 1000;
+    console.log(RomanNum);
   }
+
+  for (let i = 0; i < RomanNum.length; i++) {
+    if (RomanNum[i] >= RomanNum[i + 1]) Operation += RomanNum[i] + RomanNum[i + 1];
+    else if (RomanNum[i] <= RomanNum[i + 1]) Operation -= RomanNum[i + 1] - RomanNum[i];
+  }
+  console.log(Operation)
 };
+romanToInt("VIII")
