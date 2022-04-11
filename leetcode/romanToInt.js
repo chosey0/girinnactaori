aea75e0dcbdcb9e4a30d093a1489e9883b9d3eb5
chosey0 <csq1404@gmail.com>
2022-04-11 15:24:14 +0900

    using object for exchange roman number

--- leetcode/romanToInt.js
+++ leetcode/romanToInt.js
@@ -4,25 +4,19 @@
  */
 var romanToInt = function (s) {
   let I, V, X, L, C, D, M;
-  let n;
-  let RomanNum = s.split('');
-  let Operation = 0;
-
-  for (n = 0; n < RomanNum.length; n++) {
-    if (RomanNum[n] == 'I') RomanNum[n] = 1;
-    else if (RomanNum[n] == 'V') RomanNum[n] = 5;
-    else if (RomanNum[n] == 'X') RomanNum[n] = 10;
-    else if (RomanNum[n] == 'L') RomanNum[n] = 50;
-    else if (RomanNum[n] == 'C') RomanNum[n] = 100;
-    else if (RomanNum[n] == 'D') RomanNum[n] = 500;
-    else if (RomanNum[n] == 'M') RomanNum[n] = 1000;
-    console.log(RomanNum);
+  let result = 0;
+  const romanNum = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
   }
-
-  for (let i = 0; i < RomanNum.length; i++) {
-    if (RomanNum[i] >= RomanNum[i + 1]) Operation += RomanNum[i] + RomanNum[i + 1];
-    else if (RomanNum[i] <= RomanNum[i + 1]) Operation -= RomanNum[i + 1] - RomanNum[i];
+  for(i = 0; i<s.length; i++){
+    const excNum = romanNum[s[i]]
+    console.log(romanNum[s[i]]);
   }
-  console.log(Operation)
 };
-romanToInt("VIII")
+romanToInt("IIV")
