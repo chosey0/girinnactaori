d248cdf57d3c706e37162d552c9fbcf49d55a1ca
chosey0 <csq1404@gmail.com>
2022-04-11 15:58:03 +0900

    split for string

--- leetcode/LongestCommonPrefix.js
+++ leetcode/LongestCommonPrefix.js
@@ -2,6 +2,12 @@
  * @param {string[]} strs
  * @return {string}
  */
+// todo 주어지는 배열에 들어있는 문자열들이 공통으로 가지고 있는 연속적인 글자를 반환
+// * 쪼개고 하나씩?
 var longestCommonPrefix = function (strs) {
+  console.log(strs[0].split(''));
 
 };
+let stringArray = ["flower","flow"]
+
+longestCommonPrefix(stringArray);
